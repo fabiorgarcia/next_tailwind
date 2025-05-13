7e152ea3c896630d1c4c6fe22b3f536230697d08
Fabio Garcia <fabiorgarcia@gmail.com>
2025-05-13 15:57:19 -0300

    Criando Projeto

---
+++ next_tailwind/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-center">
+        <h1 className="text-3xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-4xl">Gerenciamento de Produtos</h1>
+      </div>
+  );
+}

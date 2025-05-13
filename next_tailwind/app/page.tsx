d1769d46ef199decbbc287514dd49c9546798cc5
Fabio Garcia <fabiorgarcia@gmail.com>
2025-05-13 18:12:57 -0300

    Home

--- next_tailwind/app/page.tsx
+++ next_tailwind/app/page.tsx
@@ -1,9 +1,28 @@
 import Image from "next/image";
+import profile from '../public/logo.svg'
 
 export default function Home() {
   return (
-    <div className="text-center">
+    <div className="flex flex-col gap-6">
+      <div className="flex gap-2 content justify-center items-center">
+        <Image
+            src={profile}
+            width={50}
+            height={50}
+            alt="Gerenciamento de Produtos"
+          />
         <h1 className="text-3xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-4xl">Gerenciamento de Produtos</h1>
       </div>
+
+      <div className="flex gap-2 content justify-center items-center">
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          required
+          value={'teste'}
+        />
+      </div>
+    </div>
   );
 }

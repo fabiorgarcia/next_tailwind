5bda4c095b627eb8f574f421f3f4535b08cabe2b
Fabio Garcia <fabiorgarcia@gmail.com>
2025-05-16 07:22:42 -0300

    Form Imput

--- next_tailwind/app/newproduct/page.tsx
+++ next_tailwind/app/newproduct/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from "next/link";
 import Header from "@/components/header";
 import Image from "next/image";
@@ -5,7 +7,35 @@ import iconhome from "@/public/home.svg"
 import ButtonSubmit from "@/components/buttonSubmit";
 import ButtonCancel from "@/components/buttonCancel";
 
+
+
+
+fetch('https://my-json-server.typicode.com/fabiorgarcia/next_tailwind/products', {
+  method: 'POST',
+  body: JSON.stringify({
+    id: '3',
+    name: 'Pneu Aro 13 165/70R13 Goodyear',
+    category: "Automotivos",
+    price: "299,90",
+    description: "Hoje tem tantos modelos que pneus que você deve ficar perdido na hora de escolher um para o seu carro. Mas nem sempre um pneu é bom para o seu carro só porque é do tamanho da sua roda. ",
+    image: "https://a-static.mlcdn.com.br/800x560/pneu-aro-13-165-70r13-goodyear-83t-touring-direction-2/magazineluiza/235820400/330721c2c460828795a086e1222d9dfb.jpg"
+
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
 export default function Product( ) {
+
+
+
+function validaForm(e:any) {
+    e.preventDefault();
+}
+  
   
   return (
     <main>
@@ -26,38 +56,40 @@ export default function Product( ) {
         </div>
         
 
-
-        <div className="flex flex-col p-8 bg-white rounded-lg gap-4 border-gray-200">
-
-          <div className="w-full p-1">
-            <label htmlFor="name" className="Tablefont-semibold text-sm font-medium text-gray-900 dark:text-white">Nome do Produto</label>
-            <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite o Nome do Produto" required />
-          </div>
-          <div className="flex-col md:flex-row flex w-full p-1 gap-4">
-            <div className="md:w-1/2 w-full">
-              <label htmlFor="category" className="Tablefont-semibold text-sm font-medium text-gray-900 dark:text-white">Categoria</label>
-              <input type="text" id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a Categoria" required />
+        <form onSubmit={(e)=> validaForm(e)}>
+          <div className="flex flex-col p-8 bg-white rounded-lg gap-4 border-gray-200">
+            
+        
+            <div className="w-full p-1">
+              <label htmlFor="name" className="Tablefont-semibold text-sm font-medium text-gray-900 dark:text-white">Nome do Produto</label>
+              <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite o Nome do Produto" required />
             </div>
-            <div className="md:w-1/2 w-full">
-              <label htmlFor="price" className="Tablefont-semibold text-sm font-medium text-gray-900 dark:text-white">Preço</label>
-              <input type="text" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a Preço" required />
+            <div className="flex-col md:flex-row flex w-full p-1 gap-4">
+              <div className="md:w-1/2 w-full">
+                <label htmlFor="category" className="Tablefont-semibold text-sm font-medium text-gray-900 dark:text-white">Categoria</label>
+                <input type="text" id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a Categoria" required />
+              </div>
+              <div className="md:w-1/2 w-full">
+                <label htmlFor="price" className="Tablefont-semibold text-sm font-medium text-gray-900 dark:text-white">Preço</label>
+                <input type="text" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a Preço" required />
+              </div>
             </div>
-          </div>
-          <div className="w-full p-1">
-            <label htmlFor="description" className="Tablefont-semibold text-sm font-medium text-gray-900 dark:text-white">Descrição</label>
-            <textarea id="description" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a descrição do produto."></textarea>
-          </div>
-          <div className="w-full p-1">
-            <label htmlFor="image" className="Tablefont-semibold text-sm font-medium text-gray-900 dark:text-white">URL da Imagem</label>
-            <input type="text" id="image" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a URL da imagem. http://..." required />
-          </div>
+            <div className="w-full p-1">
+              <label htmlFor="description" className="Tablefont-semibold text-sm font-medium text-gray-900 dark:text-white">Descrição</label>
+              <textarea id="description" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a descrição do produto."></textarea>
+            </div>
+            <div className="w-full p-1">
+              <label htmlFor="image" className="Tablefont-semibold text-sm font-medium text-gray-900 dark:text-white">URL da Imagem</label>
+              <input type="text" id="image" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a URL da imagem. http://..." required />
+            </div>
 
-          <div className="w-full p-1 flex justify-end-safe gap-4">
-            <Link href={'/'}><ButtonCancel label={'Cancelar'}/></Link> 
-            <ButtonSubmit label={'Cadastrar ✚'}/>
+            <div className="w-full p-1 flex justify-end-safe gap-4">
+              <Link href={'/'}><ButtonCancel label={'Cancelar'}/></Link> 
+              <ButtonSubmit label={'Cadastrar ✚'}/>
+            </div>
+            
           </div>
-          
-        </div>
+        </form>
 
       </div>
     </main>
--- next_tailwind/components/buttonSubmit.tsx
+++ next_tailwind/components/buttonSubmit.tsx
@@ -5,7 +5,7 @@ interface buttonProps {
 export default function ButtonSubmit({label}: buttonProps) {
 
     return (
-        <button type="button" className="md:w-auto w-full text-white bg-[#f76900] hover:bg-[#d55d05] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm py-4 px-8 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button type="submit" className="md:w-auto w-full text-white bg-[#f76900] hover:bg-[#d55d05] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm py-4 px-8 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           {label}
         </button>
     )

4f78118dfad52087cec9414e59e6c09ff5500059
Fabio Garcia <fabiorgarcia@gmail.com>
2025-05-16 11:23:37 -0300

    List All

--- next_tailwind/app/page.tsx
+++ next_tailwind/app/page.tsx
@@ -5,6 +5,7 @@ import Table from "@/components/table";
 import ButtonSubmit from "@/components/buttonSubmit";
 import { useState } from "react";
 import ButtonCancel from "@/components/buttonCancel";
+import { setDefaultAutoSelectFamily } from "net";
 
 
 const data = await fetch('https://my-json-server.typicode.com/fabiorgarcia/next_tailwind/products')
@@ -18,17 +19,13 @@ export default function Home() {
   const [priceInit, setPriceInit] = useState('');
   const [priceEnd, setPriceEnd] = useState('');
   const [listProducts, setListProducts] = useState(products);
-
   const [name, setName] = useState('');
   const [category, setCategory] = useState('');
   const [price, setPrice] = useState('');
   const [description, setDescription] = useState('');
   const [image, setImage] = useState('');
-
   const [openModal, setOpenModal] = useState(false);
 
-
-    
 
   function validaForm(e:any) {
     e.preventDefault();
@@ -44,55 +41,7 @@ export default function Home() {
     }
     setListProducts(result);
     setOpenModal(false);
-}
-
-function validaName(x:any) {
-  setName(x);
-}
-function validaCategory(x:any) {
-  setCategory(x);
-}
-function validaPrice(x:any) {
-  setPrice(x);
-}
-function validaDescription(x:any) {
-  setDescription(x);
-}
-function validaImage(x:any) {
-  setImage(x);
-}
-
-  function filterList(x:any) {
-    setNameProd(x);
-    x = x.toUpperCase();
-
-
-    if (x) {
-      var result = [];
-      var t_priceInit = 0;
-      if (priceInit) {
-
-         t_priceInit = parseInt(priceInit);
-      }
-
-      for (let i = 0; i < listProducts.length; i++) {
-        var t_name = listProducts[i].name.toUpperCase();
-        var t_price = listProducts[i].price;
-        t_price = parseInt(t_price);
-
-        if  (t_name.startsWith(x) || priceInit >= t_price) {
-          result.push(listProducts[i])
-        }
-      setListProducts(result);
-      } 
-    } else {
-      setListProducts(listAll);
-    }
-
-      
-    
   }
-
 
 
   function filterForm() {
@@ -220,25 +169,25 @@ function validaImage(x:any) {
               
                   <div className="w-full p-1">
                     <label htmlFor="name" className="Tablefont-semibold text-sm font-medium text-gray-900 dark:text-white">Nome do Produto</label>
-                    <input type="text" id="name" value={name} onChange={(e)=>validaName(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite o Nome do Produto" required />
+                    <input type="text" id="name" value={name} onChange={(e)=>setName(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite o Nome do Produto" required />
                   </div>
                   <div className="flex-col md:flex-row flex w-full p-1 gap-4">
                     <div className="md:w-1/2 w-full">
                       <label htmlFor="category" className="Tablefont-semibold text-sm font-medium text-gray-900 dark:text-white">Categoria</label>
-                      <input type="text" id="category" value={category} onChange={(e)=>validaCategory(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a Categoria" required />
+                      <input type="text" id="category" value={category} onChange={(e)=>setCategory(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a Categoria" required />
                     </div>
                     <div className="md:w-1/2 w-full">
                       <label htmlFor="price" className="Tablefont-semibold text-sm font-medium text-gray-900 dark:text-white">Preço</label>
-                      <input type="number" step="0.01" id="price" value={price} onChange={(e)=>validaPrice(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a Preço" required />
+                      <input type="number" step="0.01" id="price" value={price} onChange={(e)=>setPrice(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a Preço" required />
                     </div>
                   </div>
                   <div className="w-full p-1">
                     <label htmlFor="description" className="Tablefont-semibold text-sm font-medium text-gray-900 dark:text-white">Descrição</label>
-                    <textarea id="description" value={description} onChange={(e)=>validaDescription(e.target.value)} rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a descrição do produto."></textarea>
+                    <textarea id="description" value={description} onChange={(e)=>setDescription(e.target.value)} rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a descrição do produto."></textarea>
                   </div>
                   <div className="w-full p-1">
                     <label htmlFor="image" className="Tablefont-semibold text-sm font-medium text-gray-900 dark:text-white">URL da Imagem</label>
-                    <input type="text" id="image" value={image} onChange={(e)=>validaImage(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a URL da imagem. http://..." required />
+                    <input type="text" id="image" value={image} onChange={(e)=>setImage(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Digite a URL da imagem. http://..." required />
                   </div>
 
                   <div className="w-full p-1 flex justify-end-safe gap-4">
